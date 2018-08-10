14315f347fc9981952d4bbe0d0ffbfd951b697e2
Admin <admin@Admins-MacBook-Pro.local>
2018-08-09 20:52:41 -0700

    Writing the betting smart contract that defines how a user can bet, in the middle of writing the distribute prizes function.

---
+++ contracts/Bet.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.23;
+
+contract Bet {
+    address public owner;
+    uint256 public minimumBet;
+    uint256 public totalBet;
+    uint256 public numberOfBets;
+    uint256 public maxTime = 100;
+    uint256 public odds = 3 / 1;
+    address[] public players;
+
+    struct Player {
+        uint256 amountBet;
+        uint256 selected;
+    }
+
+    mapping(address => Player) public playerInfo;
+
+
+    function Bet(uint256 _minimumBet) public {
+        owner = msg.sender;
+        if (_minimumBet != 0) minimumBet = _minimumBet;
+    }
+
+    function kill() public {
+        if (msg.sender == owner)
+    selfdestruct(owner);
+    }
+
+    function checkPlayerExists(address player) public constant returns (bool) {
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i] == player) return true;
+        }
+        return false;
+    }
+
+    function betting(uint256 selected) public payable {
+        require(!checkPlayerExists(msg.sender));
+        require(selected >= 1 && selected <= 2);
+        require(msg.value >= minimumBet);
+
+        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].selected = selected;
+        numberOfBets++;
+        players.push(msg.sender);
+        totalBet += msg.value;
+    }
+
+
+
+    function distributePrizes(uint256 numberWinner) public {
+        address[100] memory winners;
+
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < players.length; i++) {
+            address playerAddress = players[i];
+            if (playerInfo[playerAddress].numberSelected == numberWinner) {
+                winners[count] = playerAddress;
+                count++;
+            }
+            delete playerInfo[playerAddress];
+        }
+    }
+
+}
+
